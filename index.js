978e17b7b89327992ea9e87a5cd1d03f8c76e636
Ganglians <jchav027@ucr.edu>
2019-05-28 16:19:12 -0700

    Finish working and refactor palindrome method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Export the Phrase object so it can be used in other files
 module.exports = Phrase;
 
-// Adds 'reverse' to all Strings
+// Adds 'reverse' to all Strings' prototype
 String.prototype.reverse = function() {
   
   return Array.from(this).reverse().join("");  
@@ -30,14 +30,35 @@ function Phrase(content) {
   
   this.processedContent = function processedContent() {
     
-     return this.content.toLowerCase();  
+    return this.letters().toLowerCase();  
+  }
+  
+  // Returns the letters in the content
+  // Ex. 
+  // new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    // let theLetters = [];
+    // const letterRegex = /[a-z]/i;
+
+
+    // //for(let i = 0; i < this.content.length; i ++) {
+    // Array.from(this.content).forEach(function(character) {
+    //   //let character = this.content.charAt(i);
+    //   if(character.match(letterRegex)) {
+    //     theLetters.push(character);
+    //   }
+    // });
+
+    // return theLetters.join("");
+    // //return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+    return (this.content.match(/[a-z]/ig) || []).join("");
   }
   
   // Returns true for a palindrome, false otherwise
   this.palindrome = function palindrome() {
     
-     // return this.processedContent() === reverse(this.processedContent());
-     return this.processedContent() === this.processedContent().reverse();
+    // return this.processedContent() === reverse(this.processedContent());
+    return this.processedContent() === this.processedContent().reverse();
   }
   
   // Makes the phrase LOUDER
